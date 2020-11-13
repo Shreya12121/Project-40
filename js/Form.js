df4733ebacfee9e72d62f53a33143caef6d52437
fareesa_mas <30589399+fareesa1@users.noreply.github.com>
2020-11-13 17:38:29 +0530

    Update Form.js

--- js/Form.js
+++ js/Form.js
@@ -12,12 +12,7 @@ class Form {
   }
 
   display(){
-    var title = createElement('h2')
-    title.html("Car Racing Game");
-    title.position(130, 0);
-
-    this.input.position(130, 160);
-    this.button.position(250, 200);
+    
 
     this.button.mousePressed(()=>{
       this.input.hide();
