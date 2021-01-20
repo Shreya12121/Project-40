1598430f5a06358c72095c62594533872d8e8723
fareesa_mas <30589399+fareesa1@users.noreply.github.com>
2021-01-21 04:41:51 +0530

    Update Game.js

--- js/Game.js
+++ js/Game.js
@@ -28,29 +28,5 @@ class Game {
     }
   }
 
-  play(){
-    form.hide();
-    textSize(30);
-    text("Game Start", 120, 100)
-    Player.getPlayerInfo();
-
-    if(allPlayers !== undefined){
-      var display_position = 130;
-      for(var plr in allPlayers){
-        if (plr === "player" + player.index)
-          fill("red")
-        else
-          fill("black");
-
-        display_position+=20;
-        textSize(15);
-        text(allPlayers[plr].name + ": " + allPlayers[plr].distance, 120,display_position)
-      }
-    }
-
-    if(keyIsDown(UP_ARROW) && player.index !== null){
-      player.distance +=50
-      player.update();
-    }
-  }
+  
 }
